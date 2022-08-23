a60fa2278ccf96289cfde7525c9a6f5ec600af55
Muhammad Ardhiansyah <muhammad.ardhiansyah@salt.ac.id>
2022-08-23 09:18:43 +0700

    building

--- components/_hoc/user/WithUser.jsx
+++ components/_hoc/user/WithUser.jsx
@@ -3,73 +3,75 @@ import { useEffect, useState } from "react";
 import { fetchUser } from "../../../store/actions/userAction";
 import { useDispatch } from "react-redux";
 
-const WithUser = (WrappedComponent) => (props) => {
-  const [filter, setFilter] = useState({
-    page: 1,
-    pageSize: 10,
-    gender: null,
-    keyword: null,
-    sortBy: null,
-    sortOrder: "ascend",
-    results: 30,
-    seed: "abc",
-  });
+function WithUser(WrappedComponent) {
+  return function Propsuser(props) {
+    const [filter, setFilter] = useState({
+      page: 1,
+      pageSize: 10,
+      gender: null,
+      keyword: null,
+      sortBy: null,
+      sortOrder: "ascend",
+      results: 30,
+      seed: "abc",
+    });
 
-  function handleFilter(value) {
-    const tempFilterList = { ...value };
-    setFilter(tempFilterList);
-  }
-
-  const dispatchRedux = useDispatch();
-
-  function applyFilter() {
-    let tempQueryParams = [];
-    if (filter.page) {
-      tempQueryParams = [...tempQueryParams, `page=${filter.page}`];
-    }
-    if (filter.pageSize) {
-      tempQueryParams = [...tempQueryParams, `pageSize=${filter.pageSize}`];
-    }
-    if (filter.gender) {
-      tempQueryParams = [...tempQueryParams, `gender=${filter.gender}`];
-    }
-    if (filter.keyword) {
-      tempQueryParams = [...tempQueryParams, `keyword=${filter.keyword}`];
-    }
-    if (filter.sortBy) {
-      tempQueryParams = [...tempQueryParams, `sortBy=${filter.sortBy}`];
-    }
-    if (filter.sortOrder) {
-      tempQueryParams = [...tempQueryParams, `sortOrder=${filter.sortOrder}`];
-    }
-    if (filter.results) {
-      tempQueryParams = [...tempQueryParams, `results=${filter.results}`];
-    }
-    if (filter.seed) {
-      tempQueryParams = [...tempQueryParams, `seed=${filter.seed}`];
+    function handleFilter(value) {
+      const tempFilterList = { ...value };
+      setFilter(tempFilterList);
     }
 
-    let finalQueryParams = "";
-    if (tempQueryParams.length > 0) {
-      finalQueryParams = "?" + tempQueryParams.join("&");
+    const dispatchRedux = useDispatch();
+
+    function applyFilter() {
+      let tempQueryParams = [];
+      if (filter.page) {
+        tempQueryParams = [...tempQueryParams, `page=${filter.page}`];
+      }
+      if (filter.pageSize) {
+        tempQueryParams = [...tempQueryParams, `pageSize=${filter.pageSize}`];
+      }
+      if (filter.gender) {
+        tempQueryParams = [...tempQueryParams, `gender=${filter.gender}`];
+      }
+      if (filter.keyword) {
+        tempQueryParams = [...tempQueryParams, `keyword=${filter.keyword}`];
+      }
+      if (filter.sortBy) {
+        tempQueryParams = [...tempQueryParams, `sortBy=${filter.sortBy}`];
+      }
+      if (filter.sortOrder) {
+        tempQueryParams = [...tempQueryParams, `sortOrder=${filter.sortOrder}`];
+      }
+      if (filter.results) {
+        tempQueryParams = [...tempQueryParams, `results=${filter.results}`];
+      }
+      if (filter.seed) {
+        tempQueryParams = [...tempQueryParams, `seed=${filter.seed}`];
+      }
+
+      let finalQueryParams = "";
+      if (tempQueryParams.length > 0) {
+        finalQueryParams = "?" + tempQueryParams.join("&");
+      }
+
+      dispatchRedux(fetchUser(finalQueryParams));
     }
+    useEffect(() => {
+      applyFilter();
+    }, []);
 
-    dispatchRedux(fetchUser(finalQueryParams));
-  }
-  useEffect(() => {
-    applyFilter();
-  }, []);
-
-  return (
-    <>
-      <WrappedComponent
-        {...props}
-        applyFilter={() => applyFilter()}
-        setFilter={(value) => handleFilter(value)}
-        filter={filter}
-      />
-    </>
-  );
-};
+    return (
+      <>
+        <WrappedComponent
+          {...props}
+          applyFilter={() => applyFilter()}
+          setFilter={(value) => handleFilter(value)}
+          filter={filter}
+        />
+      </>
+    );
+  };
+}
 
 export default WithUser;
